3a718660c074be11570d8337834d9841e4bbe593
dps <jennifer.dePinhoSousa@hec.de>
2025-03-12 14:55:06 +0100

    FiX

--- server/src/main/java/de/voidstack_overload/cardgame/Main.java
+++ server/src/main/java/de/voidstack_overload/cardgame/Main.java
@@ -1,11 +1,6 @@
 package de.voidstack_overload.cardgame;
 
-import de.voidstack_overload.cardgame.configuration.Config;
-import de.voidstack_overload.cardgame.network.NetworkManager;
-
 public class Main {
     public static void main(String[] args) {
-        Config.INSTANCE.init();
-        NetworkManager.INSTANCE.init();
     }
 }
