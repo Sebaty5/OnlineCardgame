64b8d675ff9dfdbdff79c66cf7ed30192efe9139
IWSC <fynn.wichary@web.de>
2025-03-12 15:46:45 +0100

    Added LoadingScreen

--- client/src/main/java/de/voidstack_overload/cardgame/application/GameManager.java
+++ client/src/main/java/de/voidstack_overload/cardgame/application/GameManager.java
@@ -1,17 +1,25 @@
 package de.voidstack_overload.cardgame.application;
+import de.voidstack_overload.cardgame.controller.LoadingScreenController;
 import de.voidstack_overload.cardgame.controller.StartupScreenController;
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 
 public class GameManager extends Application {
 
     public static Stage stage;
+
+
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -20,17 +28,18 @@ public class GameManager extends Application {
         stage.setMinWidth(800);
         stage.setMinHeight(600);
 
-        Group root = new Group();
-        Scene scene = new Scene(root, stage.getWidth(), stage.getHeight(), Color.RED);
+
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("StartupScreen.fxml"));
         Pane mainPane = fxmlLoader.load();
-        StartupScreenController controller = fxmlLoader.getController();
-
-
-
 
         stage.setScene(new Scene(mainPane, stage.getWidth(), stage.getHeight()));
+
+
+
+
+
+
 
         GameManager.stage = stage;
         GameManager.stage.show();
--- client/src/main/java/de/voidstack_overload/cardgame/controller/StartupScreenController.java
+++ client/src/main/java/de/voidstack_overload/cardgame/controller/StartupScreenController.java
@@ -1,7 +1,52 @@
 package de.voidstack_overload.cardgame.controller;
 
+import de.voidstack_overload.cardgame.application.GameManager;
+import javafx.animation.PauseTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
 public class StartupScreenController {
 
+    double progressbarValue;
+    int texturesLoaded;
+
+    @FXML
+    private void continueButtonClicked(ActionEvent event) throws Exception {
+        Stage stage = GameManager.stage;
+        texturesLoaded = 0;
+        progressbarValue = 0.0;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("LoadingScreen.fxml"));
+        Pane loadingPane = fxmlLoader.load();
+        stage.setScene(new Scene(loadingPane, stage.getWidth(), stage.getHeight()));
+
+
+        ProgressBar progressBar = (ProgressBar) stage.getScene().lookup("#LoadingBar");
+        progressBar.setProgress(0.0);
+
+        int textureAmount = 5;
+
+
+        PauseTransition pauseTransition = new PauseTransition(Duration.millis(1000));
+        pauseTransition.setOnFinished(
+                e->
+                {
+                    if(texturesLoaded < textureAmount) {
+                        //loading texture
+                        texturesLoaded++;
+                        progressbarValue = (1.0 / textureAmount) * texturesLoaded;
+                        progressBar.setProgress(progressbarValue);
+                        pauseTransition.playFromStart();
+                    }
+                }
+        );
+        pauseTransition.play();
+    }
 
 
 
