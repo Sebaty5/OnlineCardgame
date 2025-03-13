600151ae631b4af45939852a5d3082913cc83f26
dps <jennifer.dePinhoSousa@hec.de>
2025-03-13 11:06:38 +0100

    Refactored

---
+++ client/src/main/java/de/voidstack_overload/cardgame/SceneManager.java
@@ -0,0 +1,28 @@
+package de.voidstack_overload.cardgame;
+
+import de.voidstack_overload.cardgame.controller.BaseController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class SceneManager {
+    private final Stage stage;
+
+    public SceneManager(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void switchScene(String fxmlFile) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource(fxmlFile));
+        Pane root = loader.load();
+
+        BaseController controller = loader.getController();
+        controller.setSceneManager(this);
+
+        Scene scene = new Scene(root, stage.getWidth(), stage.getHeight());
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- client/src/main/java/de/voidstack_overload/cardgame/controller/LoadingScreenController.java
+++ client/src/main/java/de/voidstack_overload/cardgame/controller/LoadingScreenController.java
@@ -1,4 +1,35 @@
 package de.voidstack_overload.cardgame.controller;
 
-public class LoadingScreenController {
+import javafx.animation.PauseTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+import javafx.util.Duration;
+
+public class LoadingScreenController extends BaseController {
+
+    @FXML
+    private ProgressBar loadingBar;
+
+    private double progressbarValue = 0.0;
+    private int texturesLoaded = 0;
+
+    @FXML
+    private void initialize() {
+        loadingBar.setProgress(0.0);
+        int textureAmount = 5;
+
+        PauseTransition pauseTransition = new PauseTransition(Duration.millis(1000));
+        pauseTransition.setOnFinished(
+                e -> {
+                    if (texturesLoaded < textureAmount) {
+                        //loading texture
+                        texturesLoaded++;
+                        progressbarValue = (1.0 / textureAmount) * texturesLoaded;
+                        loadingBar.setProgress(progressbarValue);
+                        pauseTransition.playFromStart();
+                    }
+                }
+        );
+        pauseTransition.play();
+    }
 }
