c18983fa13b0d61c6d64faab94eab7fd38793579
IWSC <fynn.wichary@web.de>
2025-03-20 12:38:40 +0100

    Added Client Functions from Network Message Definitions

--- client/src/main/java/de/voidstack_overload/cardgame/connection/ConnectionManager.java
+++ client/src/main/java/de/voidstack_overload/cardgame/connection/ConnectionManager.java
@@ -1,7 +1,7 @@
 package de.voidstack_overload.cardgame.connection;
 
-import com.google.gson.Gson;
 import de.voidstack_overload.cardgame.logging.StandardLogger;
+import de.voidstack_overload.cardgame.utility.JsonBuilder;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
@@ -35,8 +35,6 @@ public class ConnectionManager {
     public boolean isConnected() {
         return this.isConnected;
     }
-
-
 
     public void connect() {
         if(isConnected ||serverUri.isEmpty()) return;
@@ -78,5 +76,76 @@ public class ConnectionManager {
 
 
 
+    public void sendMessage(String message) {
+        if (client == null || !client.isOpen()) {
+            connect();
+        }
+        if (client.isOpen()) {
+            client.send(message);
+        } else {
+            LOGGER.log("Connection not established.");
+        }
+    }
 
+    public void login(String username, String password) {
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        jsonBuilder.add("type", "ACCOUNT_LOGIN");
+        jsonBuilder.add("username", username);
+        jsonBuilder.add("password", password);
+        sendMessage(jsonBuilder.toString());
+    }
+
+    public void logout() {
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        jsonBuilder.add("type", "ACCOUNT_LOGOUT");
+        sendMessage(jsonBuilder.toString());
+    }
+
+    public void register(String username, String password) {
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        jsonBuilder.add("type", "ACCOUNT_REGISTER");
+        jsonBuilder.add("username", username);
+        jsonBuilder.add("password", password);
+        sendMessage(jsonBuilder.toString());
+    }
+
+    public void lobbyCreate(String lobbyName, String password, int maxPlayers, int botAmount) {
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        jsonBuilder.add("type", "LOBBY_CREATE");
+        jsonBuilder.add("lobbyName", lobbyName);
+        jsonBuilder.add("password", password);
+        jsonBuilder.add("maxPlayers", maxPlayers);
+        jsonBuilder.add("botCount", botAmount);
+        sendMessage(jsonBuilder.toString());
+    }
+
+    public void lobbyJoin(String lobbyID, String password) {
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        jsonBuilder.add("type", "LOBBY_JOIN");
+        jsonBuilder.add("lobbyID", lobbyID);
+        jsonBuilder.add("lobbyPassword", password);
+        sendMessage(jsonBuilder.toString());
+    }
+
+    public void lobbyLeave() {
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        jsonBuilder.add("type", "LOBBY_LEAVE");
+        sendMessage(jsonBuilder.toString());
+    }
+
+    public void lobbyUpdate(String lobbyName, String lobbyPassword, int maxPlayers, int botAmount) {
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        jsonBuilder.add("type", "LOBBY_UPDATE");
+        jsonBuilder.add("lobbyName", lobbyName);
+        jsonBuilder.add("lobbyPassword", lobbyPassword);
+        jsonBuilder.add("maxPlayers", maxPlayers);
+        jsonBuilder.add("botCount", botAmount);
+        sendMessage(jsonBuilder.toString());
+    }
+
+    public void lobbyList() {
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        jsonBuilder.add("type", "LOBBY_LIST");
+        sendMessage(jsonBuilder.toString());
+    }
 }
