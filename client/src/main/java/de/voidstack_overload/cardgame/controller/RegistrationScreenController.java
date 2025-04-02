b375421eb8401e7dc8412fb64d13eae5692996e1
dps <jennifer.dePinhoSousa@hec.de>
2025-04-02 14:04:21 +0200

    VSO-73

---
+++ client/src/main/java/de/voidstack_overload/cardgame/controller/RegistrationScreenController.java
@@ -0,0 +1,25 @@
+package de.voidstack_overload.cardgame.controller;
+
+import de.voidstack_overload.cardgame.connection.ConnectionManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegistrationScreenController extends BaseController {
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    public void confirmRegistration() {
+        ConnectionManager.getInstance().register(username.getText(), password.getText());
+    }
+}
