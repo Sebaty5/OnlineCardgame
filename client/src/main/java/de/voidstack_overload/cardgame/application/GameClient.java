95db41db42404adc39b7c7e9310155720484e10a
Sebaty5 <sebaty2@gmail.com>
2025-05-06 10:15:40 +0200

    Added event handle to shut down all tasks when user closes the window (pressing the X or Alt+F4)

--- client/src/main/java/de/voidstack_overload/cardgame/application/GameClient.java
+++ client/src/main/java/de/voidstack_overload/cardgame/application/GameClient.java
@@ -3,7 +3,9 @@ package de.voidstack_overload.cardgame.application;
 import de.voidstack_overload.cardgame.SceneFXML;
 import de.voidstack_overload.cardgame.SceneManager;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class GameClient extends Application {
     private static SceneManager sceneManager;
@@ -15,6 +17,10 @@ public class GameClient extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         sceneManager = new SceneManager(primaryStage);
+        primaryStage.setOnCloseRequest((WindowEvent evt) -> {
+            Platform.exit();
+            System.exit(0);
+        });
 
         primaryStage.setTitle("Durak");
         primaryStage.setResizable(false);
