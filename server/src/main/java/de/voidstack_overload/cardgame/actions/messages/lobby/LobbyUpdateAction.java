72ce564465dd8cfc035022e55a28a0a4556569cf
Sebaty5 <sebaty2@gmail.com>
2025-05-06 21:45:17 +0200

    Fixed a miss copied part and added a TODO once the lobby update methods become relevant.

--- server/src/main/java/de/voidstack_overload/cardgame/actions/messages/lobby/LobbyUpdateAction.java
+++ server/src/main/java/de/voidstack_overload/cardgame/actions/messages/lobby/LobbyUpdateAction.java
@@ -2,9 +2,6 @@ package de.voidstack_overload.cardgame.actions.messages.lobby;
 
 import com.google.gson.JsonObject;
 import de.voidstack_overload.cardgame.messages.OutgoingMessageType;
-import de.voidstack_overload.cardgame.game.lobby.LobbyManager;
-import de.voidstack_overload.cardgame.network.UserManager;
-import de.voidstack_overload.cardgame.network.User;
 import de.voidstack_overload.cardgame.network.Response;
 import de.voidstack_overload.cardgame.utility.ResponseBuilder;
 import org.java_websocket.WebSocket;
@@ -16,13 +13,7 @@ public class LobbyUpdateAction extends LobbyAction {
         if(response != null) {
             return response;
         }
-
-        User user = UserManager.INSTANCE.getUser(connection);
-        if(!LobbyManager.INSTANCE.isPlayerInLobby(user)) {
-            return ResponseBuilder.errorResponse("Player is not in a lobby");
-        }
-
-        LobbyManager.INSTANCE.leaveLobby(user);
-        return ResponseBuilder.build(OutgoingMessageType.LOBBY_LEAVE_ACKNOWLEDGE);
+        // TODO: Add actual lobby update handling
+        return ResponseBuilder.build(OutgoingMessageType.LOBBY_UPDATE_DENY);
     }
 }
